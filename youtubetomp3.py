91ecaf65a2913e30ad0eab48e65f416891690561
Noel <helloitsnoel@gmail.com>
2020-03-24 20:45:24 +1000

    Embedding image / thumbnail onto mp3 file.
    Adding some code that downloads the original thumbnail from YouTube, embeds that thumbnail onto the .mp3 file; and deletes the downloaded image as it's not needed anymore. Also added comments to sort out code and renamed confusing variable names :)

--- youtubetomp3.py
+++ youtubetomp3.py
@@ -3,25 +3,71 @@
 from pytube import YouTube
 import os
 from moviepy.editor import *
+import eyed3
+import requests
 
-link = input("Enter The YouTube Link { with https } : ")
+#--------- DOWNLOADING AUDIO FROM YOUTUBE -----------------
+# user inputs
+yt_link = input("Enter The YouTube Link { with https } : ")
 path1 = input("Enter The Download Path : ")
-name = input("Enter The Name Of Music You Want To Given : ")
 
-yt = YouTube(link)
-print(yt.title + "\n") 
+# downloading video using pytube
+yt = YouTube(yt_link)
+# now we have a YouTube object called yt
+# printing the title of the video to terminal screen
+song_name = yt.title
+print(song_name)
+img_id = yt.thumbnail_url
+# see available media formats
 yt.streams.all()
+# getting the first stream option
 stream = yt.streams.first()
+# downloading video to path2
 path2 = stream.download(path1)
-path3 = path1 + "/" +  name + ".mp3"
-
 mp4_file = path2
+# creating a path for mp3 file
+path3 = path1 + "/" +  song_name + ".mp3"
 mp3_file = path3
 
-videoclip = VideoFileClip(mp4_file)
-audioclip = videoclip.audio
-audioclip.write_audiofile(mp3_file)
+# conversion from mp4 to mp3
+videoclip = VideoFileClip(mp4_file) # create a videoclip object
+audioclip = videoclip.audio # get the audio object from mp4 file
+audioclip.write_audiofile(mp3_file) # save the audio of mp4 into an mp3 file
 audioclip.close()
 videoclip.close()
 
 os.remove(path2)
+
+#--------- DOWNLOADING YOUTUBE THUMBNAIL IMAGE -----------------
+
+# message confirming that the img download process has started
+print('Importing youtube thumbnail...')
+
+url = img_id
+r = requests.get(url)
+
+# assigning a path for the temporary image file
+# (it's going to get deleted after embed onto mp3)
+path4 = path1 + "/img" + ".jpg"
+
+with open(path4, 'wb') as f:
+    f.write(r.content)
+
+# retrieving HTTP meta-data
+print(r.status_code)
+print(r.headers['content-type'])
+print(r.encoding)
+
+#--------- EMBEDDING THUMBNAIL IMG ONTO MP3 FILE -----------------
+
+audiofile = eyed3.load(mp3_file)
+if (audiofile.tag == None):
+    audiofile.initTag()
+
+audiofile.tag.images.set(3, open(path4,'rb').read(), 'image/jpeg')
+
+audiofile.tag.save()
+
+# deleting the thumbnail as it's not needed anymore
+os.remove(path4)
+print("Song downloaded successfully :)")
